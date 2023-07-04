c2cc75cdb0cb09c92badf6f0f93a9d27574ad634
Heejun-0219 <heejun0219@gmail.com>
2023-07-04 19:28:09 +0900

    refactor

--- srcs/init.c
+++ srcs/init.c
@@ -2,6 +2,26 @@
 
 int g_exit_code;
 
+void    sig_handler(int signo)
+{
+    (void)signo;
+	g_exit_code = 1;
+	printf("\n");
+	if (rl_on_new_line() == -1)
+		exit(1);
+	rl_replace_line("", 1);
+	rl_redisplay();
+}
+
+void    init_sig(t_info *info)
+{
+    info->term = info->term_back;
+    info->term.c_lflag = ~ECHOCTL;
+    tcsetattr(STDIN_FILENO, TCSANOW, &info->term);
+    signal(SIGINT, sig_handler);
+    signal(SIGQUIT, SIG_IGN);
+}
+
 void    init_env_list(t_info *info, char **env)
 {
     size_t  i;
--- srcs/main.c
+++ srcs/main.c
@@ -6,26 +6,6 @@ int	ft_error(char *m, int error)
 	return (error);
 }
 
-void    sig_handler(int signo)
-{
-    (void)signo;
-	g_exit_code = 1;
-	printf("\n");
-	if (rl_on_new_line() == -1)
-		exit(1);
-	rl_replace_line("", 1);
-	rl_redisplay();
-}
-
-void    init_sig(t_info *info)
-{
-    info->term = info->term_back;
-    info->term.c_lflag = ~ECHOCTL;
-    tcsetattr(STDIN_FILENO, TCSANOW, &info->term);
-    signal(SIGINT, sig_handler);
-    signal(SIGQUIT, SIG_IGN);
-}
-
 int main(int ac, char **av, char **env)
 {
     char *line;
