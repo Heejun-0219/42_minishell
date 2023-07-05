4b934bd2efebc52d78d9283e5ba268fe64f8c49f
LeeMinjea <gabe0529@naver.com>
2023-07-05 12:08:50 +0900

    FIX : SIGABRT 오류 수정

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,13 +6,13 @@
 /*   By: mi <mi@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 23:41:24 by mi                #+#    #+#             */
-/*   Updated: 2023/07/05 02:41:40 by mi               ###   ########.fr       */
+/*   Updated: 2023/07/05 12:07:44 by mi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static t_token	set_token(t_parse *parse, char *token_str, int index)
+static t_token	set_token(char *token_str, int index)
 {
 	t_token new_token;
 	
@@ -85,15 +85,15 @@ void tokenize_line(t_parse *parse) // 라인을 쪼개서, 토큰화 시킴
 
 	num_tokens = 0;
 	i = 0;
-	tokens_str = ft_split(parse->line, " "); // TODO : 나중에 free 해줘야함
+	tokens_str = ft_split(parse->line, ' '); // TODO : 나중에 free 해줘야함
 	merge_quoted_tokens(tokens_str);
 	while (tokens_str[num_tokens] != NULL)
 		num_tokens++;														   // 토큰 개수 세기
-	parse->tokens = ft_realloc(parse->tokens, sizeof(t_token) * (num_tokens)); // 토큰 개수만큼 메모리 할당
+	parse->tokens = ft_malloc(sizeof(t_token) * (num_tokens)); // 토큰 개수만큼 메모리 할당
 	parse->token_count = num_tokens;
 	while (i < num_tokens)
 	{
-		parse->tokens[i] = set_token(parse, tokens_str[i], i); // 각각 토큰 타입, 토큰 인덱스, 토큰 문자열 설정 후 토큰 배열에 넣기
+		parse->tokens[i] = set_token(tokens_str[i], i); // 각각 토큰 타입, 토큰 인덱스, 토큰 문자열 설정 후 토큰 배열에 넣기
 		i++;
 	}
 }
