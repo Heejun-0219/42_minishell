4da3fc20e6da2832aab46519b21cc8ee11809eea
hj <heejun0219@gmail.com>
2023-07-06 14:55:04 +0900

    fix: norm

--- srcs/builtin/exe_pwd.c
+++ srcs/builtin/exe_pwd.c
@@ -6,20 +6,19 @@
 /*   By: heejunki <heejunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 14:50:23 by heejunki          #+#    #+#             */
-/*   Updated: 2023/07/06 14:51:42 by heejunki         ###   ########.fr       */
+/*   Updated: 2023/07/06 14:54:39 by heejunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int exe_pwd(void)
+int	exe_pwd(void)
 {
-    char    pwd[256];
+	char	pwd[256];
 
-    if (getcwd(pwd, 256))
-        printf("%s\n", pwd);
-    else
-        exit(EXIT_FAILURE);
-    exit(EXIT_SUCCESS);
+	if (getcwd(pwd, 256))
+		printf("%s\n", pwd);
+	else
+		exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
-    
